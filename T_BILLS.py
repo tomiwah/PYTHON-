075924fe6cd3fb64f677a51aa22756bd24102f7e
tomiwah <tomiwaojobe@gmail.com>
2017-07-13 22:16:51 +0100

    update

--- T_BILLS.py
+++ T_BILLS.py
@@ -1,5 +1,13 @@
-profit=[]
+#profit=[]
+#take_home = []
+
 def t_bills(amount, days, rate):
     profit = (days/365) * (rate/100) * amount
-    return profit
-print (profit)
+    take_home = (amount) + (profit)
+    print (profit)
+    print(' you will be paid {} Naira'.format(take_home))
+    #return profit, take_home
+
+
+    
+
