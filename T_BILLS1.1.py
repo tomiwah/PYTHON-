836a7faf70c028e3439085359632dc9b106fb8a9
tomiwah <tomiwaojobe@gmail.com>
2017-12-17 21:30:43 +0100

    date format update

--- T_BILLS1.1.py
+++ T_BILLS1.1.py
@@ -1,6 +1,7 @@
 rate = 12.5
 from datetime import datetime
 from datetime import timedelta
+from datetime import date
 
 
 
@@ -8,14 +9,14 @@ print("Hello Investor, how much would you like to invest in the treasury bills?"
 amount=int(input())
 print("how many days wll you like your investment to run for?")
 no_days = int(input())
-date = datetime.now() + timedelta(days=no_days)
+date = date.today() + timedelta(days=no_days)
 d = (no_days/365)
 r = (rate/100)
 interest = d * r * amount
 print(interest)
 Total = amount + interest
 print("the current interest rate today is " + str(rate) + "%")
-print("your investment will mature on the " + str(date))
+print("your investment will mature on the " + str(date.strftime('%d %b, %Y')))
 print("your interest will be " + str(interest))
 print("Total amount paid into your account will be " + str(Total))
 
